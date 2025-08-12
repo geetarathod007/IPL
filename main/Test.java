0359fe6caf8cacaeee5f4ef3f4db74cca8d7fc80
ASUS <ASUS@LAPTOP-1NB697I5>
2025-08-12 19:00:01 +0530

    My Second Commit

--- main/Test.java
+++ main/Test.java
@@ -3,7 +3,7 @@ import service.TeamService;
 public class Test {
  public static void main(String[] args) {
 	 TeamService service = new TeamService();
-	 service.printTeamDetails();
+	 service.println("Test github");
 	 
  }
 }
